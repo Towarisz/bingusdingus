77c35a43dc5039959fd3a5ee381f1cd921d8b90d
Towarisz <80888082+Towarisz@users.noreply.github.com>
2022-12-14 21:18:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let e=100;
 setInterval(()=>{
-  document.querySelectorAll("*").forEach(d=>d.style.transform=`scale(${e+=1}%,${e}%)`)}
-            if(e>250) e=100;
+  document.querySelectorAll("*").forEach(d=>d.style.transform=`scale(${e+=1}%,${e}%)`)
+            if(e>250) e=100;}
             ,100)
 
